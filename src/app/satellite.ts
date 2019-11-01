435a924643078bba66413526dbde900a85e53a1d
mojoco85 <mojoco85@outlook.com>
2019-10-31 21:55:47 -0500

    ready for submission

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,18 @@
 export class Satellite {
+
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    shouldShowWarning: boolean;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean, shouldShowWarning: boolean){
+        this.name = name
+        this.orbitType = orbitType
+        this.type = type
+        this.operational = operational
+        this.launchDate = launchDate
+        this.shouldShowWarning = shouldShowWarning
+    }
 }
